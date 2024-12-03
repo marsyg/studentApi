82fa552900dc27d2cb9ddff10d0873fe6beb25c4
marsyg <net.maaz@yahoo.com>
2024-12-03 09:56:52 +0530

    graceful shutdown

--- cmd/student-api/main.go
+++ cmd/student-api/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/marsyg/studentApi/internal/config"
 )
@@ -21,8 +27,21 @@ func main() {
 		Handler: router,
 	}
 	fmt.Printf("server is started at :%s", cfg.HTTP_SERVER.Addr)
-	err := server.ListenAndServe()
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("Filed to start server ")
+		}
+	}()
+	<-done
+	slog.Info("Shutting down the server")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := server.Shutdown(ctx)
 	if err != nil {
-		log.Fatal("Filed to start server ")
+		slog.Error("failed tyo shutdown the server ", slog.String("error", err.Error()))
 	}
+	slog.Info("server shutdown sucessfully")
 }
